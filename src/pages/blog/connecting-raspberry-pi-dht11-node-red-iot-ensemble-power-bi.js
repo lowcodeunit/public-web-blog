a28b88d8de4734df032ba9ac5be9331d53e9fa54
mcgear <michael.gearhardt@gmail.com>
2021-01-25 10:34:04 -0700

    Old redirect

---
+++ src/pages/blog/connecting-raspberry-pi-dht11-node-red-iot-ensemble-power-bi.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Redirect} from '@docusaurus/router';
+
+const OldRedirect = () => {
+  return <Redirect to="raspberry-pi-dht11-node-red-iot-ensemble-power-bi" />;
+};
+
+export default OldRedirect;
