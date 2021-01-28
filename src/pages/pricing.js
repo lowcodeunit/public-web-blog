bcee78d029d2ef867a99c40b2ae3a4637bd61460
mcgear <michael.gearhardt@gmail.com>
2021-01-27 18:02:01 -0700

    Get billing working

--- src/pages/pricing.js
+++ src/pages/pricing.js
@@ -10,22 +10,30 @@ import pricingImagePro from '../../static/img/pricing-page-pro.png';
 import styles from './styles.module.css';
 
 function Pricing() {
-  const lcu = JSON.stringify({ State: { ActionRoot: '/api/state', Root: '/api/state' } });
+  // const lcu = JSON.stringify({ State: { ActionRoot: '/api/state', Root: '/api/state' } });
 
-  const script = document.getElementById('lcu-reg') || document.createElement('script');
+  // const script = document.getElementById('lcu-reg') || document.createElement('script');
 
-  script.id = 'lcu-reg';
+  // script.id = 'lcu-reg';
 
-  script.innerHTML = `window.LCU=${lcu};`;
+  // script.innerHTML = `window.LCU=${lcu};`;
 
-  if (!document.getElementById('lcu-reg')) {
-    document.body.appendChild(script);
-  }
+  // if (!document.getElementById('lcu-reg')) {
+  //   document.body.appendChild(script);
+  // }
+
+  const lcuStr = JSON.stringify({ State: { ActionRoot: '/api/state', Root: '/api/state' } });
 
   return (
     <Layout title="Pricing">
       <Helmet>
         <script src="https://www.iot-ensemble.com/billing/lcu/wc/lcu-billing.lcu.js" type="text/javascript" />
+
+        <script type="text/javascript">
+          {`
+            window.LCU = ${lcuStr};
+          `}
+        </script>
       </Helmet>
 
       <header className={clsx('hero hero--primary', styles.heroBanner)}>
@@ -39,17 +47,15 @@ function Pricing() {
       <div style={{ display: 'flex', alignContent: 'center', justifyContent: 'center', margin: '2em' }}>
         <div style={{ width: '275px', height: '422.5px', margin: '2em 1em' }}>
           <a href="https://www.iot-ensemble.com/dashboard">
-            <img src={pricingImageFree} class="pricing-image" />
+            <img src={pricingImageFree} className="pricing-image" />
           </a>
         </div>
 
-        <div style={{ margin: '2em 1em' }}>
-          <lcu-billing-plan-view-element plan-group="iot"></lcu-billing-plan-view-element>
-        </div>
+        <div style={{ margin: '2em 1em' }}>{/* <lcu-billing-plan-view-element plan-group="iot"></lcu-billing-plan-view-element> */}</div>
 
         <div style={{ width: '275px', height: '422.5px', margin: '2em 1em' }}>
           <a href="mailto: sales@fathym.com">
-            <img src={pricingImageEnt} class="pricing-image" />
+            <img src={pricingImageEnt} className="pricing-image" />
           </a>
         </div>
       </div>
