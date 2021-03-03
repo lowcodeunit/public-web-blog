027ee45489a7dfb86d83b55efe5223687afebc43
Jack Penhale <jack.penhale@fathym.com>
2021-03-03 09:55:16 -0700

    url shortening

--- src/pages/connecting-raspberry-pi-dht11-node-red-iot-ensemble-power-bi.js
+++ src/pages/connecting-raspberry-pi-dht11-node-red-iot-ensemble-power-bi.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Redirect} from '@docusaurus/router';
 
 const OldRedirect = () => {
-  return <Redirect to="raspberry-pi-dht11-node-red-iot-ensemble-power-bi" />;
+  return <Redirect to="raspberry-pi-iot-ensemble-power-bi" />;
 };
 
 export default OldRedirect;
