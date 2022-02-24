186a9bdf697ce5455ed7dae08eb5056e21755f94
Jeremy Tomlinson <jeremy.tomlinson@gmail.com>
2022-02-24 12:01:38 -0700

    Redirect is working for blog home

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Redirect} from '@docusaurus/router';
+
+const Home = () => {
+  return <Redirect to="articles/2022/february/2022-02-20-angular-vs-react-whichever-you-choose-you-can-use-at-fathym" />;
+};
+
+export default Home;
