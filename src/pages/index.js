2fb37391bfb9909f25e625b7367179969ba910a3
mcgear <michael.gearhardt@gmail.com>
2022-03-02 13:17:23 -0700

    useEffect

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,10 @@ import React, { useEffect } from 'react';
 import { Redirect } from '@docusaurus/router';
 
 const Home = () => {
-  useEffect(() => {
-    let redirect =
-      'articles/2022/february/2022-02-28-angular-vs-react-vs-vue-you-choose';
+  let redirect =
+    'articles/2022/february/2022-02-28-angular-vs-react-vs-vue-you-choose';
 
+  useEffect(() => {
     if (!window.location.pathname.endsWith('/')) {
       redirect = `/${redirect}`;
     }
