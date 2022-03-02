c2bc8a59ad5307c10ff97e5de490f85d1bfdbc87
mcgear <michael.gearhardt@gmail.com>
2022-03-02 12:42:12 -0700

    try with useEffect

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Redirect } from '@docusaurus/router';
 
 const Home = () => {
-  let redirect =
-    'articles/2022/february/2022-02-28-angular-vs-react-vs-vue-you-choose';
+  useEffect(() => {
+    let redirect =
+      'articles/2022/february/2022-02-28-angular-vs-react-vs-vue-you-choose';
 
-  if (!location.pathname.endsWith('/')) {
-    redirect = `/${redirect}`;
-  }
+    if (!location.pathname.endsWith('/')) {
+      redirect = `/${redirect}`;
+    }
+  });
 
   return <Redirect to={redirect} />;
 };
